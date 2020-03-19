6351655f1aa5447964d405225d055a8227e74f81
xprmntlst <58903165+xprmntlst@users.noreply.github.com>
2020-03-19 06:41:01 +0530

    Some more changes after reloading

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla</h1>
 <div>
- you can find more info about Parcel
+  find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
