0795938d5f04f40eb406957f41db3fa3dbf1faaf
xprmntlst <58903165+xprmntlst@users.noreply.github.com>
2020-03-19 06:47:49 +0530

    Test after renaming cs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Again!</h1>
 <div>
-  find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  testing this branch !
 </div>
 `;
