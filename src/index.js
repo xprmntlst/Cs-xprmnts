20996b98291a8381603d7022255ced8c02a7670d
xprmntlst <58903165+xprmntlst@users.noreply.github.com>
2020-03-20 08:42:29 +0530

    Another Commit
     Next Day

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Again!</h1>
 <div>
-  I dont think even keyboard is needed !!!
-  (its needed if u want to use intellisense)
+  just wanted to say hi again !
 </div>
 `;
